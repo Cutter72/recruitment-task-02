62d991001eba39e38eb560a662896b17c34c0d2e
Cutter72 <drelich_pawel@o2.pl>
2019-10-21 14:15:55 +0200

    log4j added

--- src/main/java/pl/spotify/task/demo/controller/HomeController.java
+++ src/main/java/pl/spotify/task/demo/controller/HomeController.java
@@ -2,6 +2,7 @@ package pl.spotify.task.demo.controller;
 
 import com.wrapper.spotify.model_objects.specification.Artist;
 import com.wrapper.spotify.model_objects.specification.Track;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import pl.spotify.task.demo.service.SpotifyAuthentication;
 @Controller
 public class HomeController {
     private SpotifyAuthentication spotifyAuthentication;
+    final static Logger log = Logger.getLogger(HomeController.class);
 
     public HomeController(SpotifyAuthentication spotifyAuthentication) {
         this.spotifyAuthentication = spotifyAuthentication;
@@ -31,8 +33,10 @@ public class HomeController {
     @GetMapping("/searchTracks")
     public String searchTracks(Model model, @RequestParam(required = false) String query) {
         if (query == null) {
+            log.info("Search for tracks with null query input.");
             return "searchTracks";
         }
+        log.info("Search for tracks with '" + query + "' qury input.");
         SearchTracks.setParams(SpotifyAuthentication.accessToken, query, 0, 30);
         Track[] tracks = SearchTracks.searchTracks_Sync();
         model.addAttribute("searchedTracks", tracks);
@@ -43,8 +47,10 @@ public class HomeController {
     @GetMapping("/searchArtists")
     public String searchArtists(Model model, @RequestParam(required = false) String query) {
         if (query == null) {
+            log.info("Search for artists with null query input.");
             return "searchArtists";
         }
+        log.info("Search for artists with '" + query + "' qury input.");
         SearchArtists.setParams(SpotifyAuthentication.accessToken, query, 0, 30);
         Artist[] artists = SearchArtists.searchArtists_Sync();
         model.addAttribute("searchedArtists", artists);
